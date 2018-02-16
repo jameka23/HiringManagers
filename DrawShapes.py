0114d75af126d7b19e7b4351b343da0828189e17
Jameka Echols <jameka.echols>
2018-02-15 20:00:11 -0600

    e

--- DrawShapes.py
+++ DrawShapes.py
@@ -1,4 +1,9 @@
 import turtle
+def drawSq(the_turtle):
+    for x in range(1,5):
+        the_turtle.forward(100)
+        the_turtle.right(90)
+        
 def draw_square():
     window = turtle.Screen() 
     window.bgcolor("yellow") #turns the shell's window yellow
@@ -7,27 +12,29 @@ def draw_square():
     brad.shape("turtle")
     brad.color("blue")
     brad.speed(2)
-    for x in range(1,5):
-        brad.forward(100)
-        brad.right(90)
+ 
+    for i in range(1,36):
+        drawSq(brad)
+        brad.right(10)
+
     
 
 
-    angie = turtle.Turtle() #init
-    angie.shape("arrow")
-    angie.color("black")
-    angie.circle(100)
-
-    
-    billie = turtle.Turtle()
-    billie.shape("classic")
-    billie.color("purple")
-    billie.forward(100)
-    billie.right(45)
-    billie.backward(100)
-    billie.right(65)
-    billie.forward(75)
-    
+##    angie = turtle.Turtle() #init
+##    angie.shape("arrow")
+##    angie.color("black")
+##    angie.circle(100)
+##
+##    
+##    billie = turtle.Turtle()
+##    billie.shape("classic")
+##    billie.color("purple")
+##    billie.forward(100)
+##    billie.right(45)
+##    billie.backward(100)
+##    billie.right(65)
+##    billie.forward(75)
+##    
     window.exitonclick()
 draw_square()
 

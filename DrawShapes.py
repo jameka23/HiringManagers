529aef63a427dc01cae412bf5a5e2df90b311c9d
Jameka Echols <jameka.echols>
2018-02-15 19:53:48 -0600

    added more features such as drawing a triangle

--- DrawShapes.py
+++ DrawShapes.py
@@ -4,14 +4,30 @@ def draw_square():
     window.bgcolor("yellow") #turns the shell's window yellow
 
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(2)
+    for x in range(1,5):
+        brad.forward(100)
+        brad.right(90)
+    
 
+
+    angie = turtle.Turtle() #init
+    angie.shape("arrow")
+    angie.color("black")
+    angie.circle(100)
+
+    
+    billie = turtle.Turtle()
+    billie.shape("classic")
+    billie.color("purple")
+    billie.forward(100)
+    billie.right(45)
+    billie.backward(100)
+    billie.right(65)
+    billie.forward(75)
+    
     window.exitonclick()
 draw_square()
+
